5e82450a7937d09514e2cfe5176ba824bf89d24c
DomoChip <domochip@hotmail.com>
2022-01-29 22:54:10 +0100

    Bump Version

--- src/Main.h
+++ src/Main.h
@@ -15,7 +15,7 @@
 #define APPLICATION1_DESC "DomoChip Wireless TeleInfo"
 #define APPLICATION1_CLASS WebTeleInfo
 
-#define VERSION_NUMBER "3.4.0"
+#define VERSION_NUMBER "3.4.1"
 
 #define DEFAULT_AP_SSID "WirelessTeleInfo"
 #define DEFAULT_AP_PSK "PasswordTeleInfo"
