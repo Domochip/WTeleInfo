5c31be192dbf29e02fdda7acef062f7e7ca23493
DomoChip <domochip@hotmail.com>
2022-09-03 14:46:11 +0200

    remove firmware

--- rename_firmware.py
+++
@@ -1,7 +0,0 @@
-Import("env")
-
-my_flags = env.ParseFlags(env['BUILD_FLAGS'])
-defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
-# print defines
-
-env.Replace(PROGNAME=defines.get("MODEL"))
--- src/base/Version.h
+++
@@ -1,6 +0,0 @@
-#ifndef Version_h
-#define Version_h
-
-#define BASE_VERSION "1.4.1"
-
-#endif
